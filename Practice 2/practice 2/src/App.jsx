1ebd03ba0dd046702e7aa66a327232c82f499481
Your Name <sheikhabdullah456677@gmail.com>
2025-02-04 03:15:10 +0500

    commit

--- Practice 2/practice 2/src/App.jsx
+++ Practice 2/practice 2/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import Hero from './Hero.jsx/Hero'
+import  Footer from './Components/Footer/index.jsx'
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      {/* <Navbar/>
+      <Hero/> */}
+      <Footer/>
     </div>
   )
 }
